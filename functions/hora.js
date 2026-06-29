29ddda67d8c7693c2a555d6c3b7984dc57c4f370
allnmaxmo <allnmaxmo@gmail.com>
2026-06-29 17:42:34 -0300

    remoção da pasta functions

--- functions/hora.js
+++
@@ -1,9 +0,0 @@
-const URL_DO_WORKER = "https://projetinhoestudocloud-api.allnmaxmo.workers.dev/";
-
-export async function onRequest(context) {
-  const urlDaPagina = new URL(context.request.url);
-  const urlDaHora = new URL("/hora", URL_DO_WORKER);
-  urlDaHora.search = urlDaPagina.search;
-
-  return fetch(urlDaHora.toString());
-}
--- functions/sorteio.js
+++
@@ -1,7 +0,0 @@
-const URL_DO_WORKER = "https://projetinhoestudocloud-api.allnmaxmo.workers.dev/";
-
-export async function onRequest() {
-  const urlDoSorteio = new URL("/sorteio", URL_DO_WORKER);
-
-  return fetch(urlDoSorteio.toString());
-}
